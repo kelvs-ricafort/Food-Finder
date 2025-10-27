388099c30a3284ce5bb9592acb336e93564ddabd
Kelvin Campos Ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2025-10-27 20:10:54 +0800

    - Add iOS Project

---
+++ ios/FoodFinder/FoodFinder/FoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFinderApp.swift
+//  FoodFinder
+//
+//  Created by Kelvin Ricafort on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
